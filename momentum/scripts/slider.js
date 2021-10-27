131de1ca2842a4920cdcf92b58c07e539b2294f1
whispermind <49441048+whispermind@users.noreply.github.com>
2021-10-27 22:01:49 +0300

    fix: replace fetch

--- momentum/scripts/slider.js
+++ momentum/scripts/slider.js
@@ -1,18 +1,17 @@
 import { getDayTime } from "./greetings.js";
+const SLIDER_NAV = document.querySelector('.slider-icons');
+let unsplash = null;
+let flickr = null;
 export default async function slider(update) {
-  let response = null;
-  let collection = null;
-  const SLIDER_NAV = document.querySelector('.slider-icons');
+  const { settings } = await import('./state.js');
+  if (settings.api === 'unsplash') unsplash = await getData(`https://api.unsplash.com/search/photos/?client_id=SaIx04gu2ik9Jsm_ExeT4Ah7u5sXqhPnd7bo-oZouyA&orientation=landscape&query=${settings.tags ? settings.tags : getDayTime()}&per_page=20`);
+  if (settings.api === 'flickr') flickr = await getData(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f1d1e25e63b77ed91af6b5fe0b0a65ac&tags=${settings.tags ? settings.tags : getDayTime()}&extras=url_l&format=json&nojsoncallback=1&per_page=20`);
   let processed = false;
   if (!update) {
     SLIDER_NAV.dataset.current = getRandomInt(0, 19);
     setImage();
     SLIDER_NAV.addEventListener('click', async function (event) {
       if (processed) return
-      let { settings } = await import("./state.js");
-      if (settings.api === 'unsplash') response = await getData(`https://api.unsplash.com/search/photos/?client_id=SaIx04gu2ik9Jsm_ExeT4Ah7u5sXqhPnd7bo-oZouyA&orientation=landscape&query=${settings.tags ? settings.tags : getDayTime()}&per_page=20`);
-      if (settings.api === 'flickr') response = await getData(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f1d1e25e63b77ed91af6b5fe0b0a65ac&tags=${settings.tags ? settings.tags : getDayTime()}&extras=url_l&format=json&nojsoncallback=1&per_page=20`);
-      collection = response?.photos?.photo || response?.results
       processed = true;
       if (event.target.classList.contains('slide-prev')) SLIDER_NAV.dataset.current--
       if (event.target.classList.contains('slide-next')) SLIDER_NAV.dataset.current++
@@ -21,14 +20,15 @@ export default async function slider(update) {
       setImage();
     });
   }
-
-  function setImage() {
+  async function setImage() {
+    const flickrCollection = flickr?.photos.photo;
+    const unsplashCollection = unsplash?.results;
     const img = document.createElement('img');
     let url = null;
-    if (collection) url = collection[SLIDER_NAV.dataset.current]?.url_l || collection[SLIDER_NAV.dataset.current]?.urls?.full
-    if (!url) {
-      url = `https://raw.githubusercontent.com/whispermind/stage1-tasks/assets/images/${getDayTime()}/${SLIDER_NAV.dataset.current < 9 ? '0' + (Number(SLIDER_NAV.dataset.current) + 1) : Number(SLIDER_NAV.dataset.current) + 1}.jpg`;
-    }
+    if (settings.api === 'unsplash') url = unsplashCollection[SLIDER_NAV.dataset.current].urls.regular;
+    if (settings.api === 'flickr') url = flickrCollection[SLIDER_NAV.dataset.current].url_l;
+    if (settings.api === 'git') url = `https://raw.githubusercontent.com/whispermind/stage1-tasks/assets/images/${getDayTime()}/${SLIDER_NAV.dataset.current < 9 ? '0' + (Number(SLIDER_NAV.dataset.current) + 1) : Number(SLIDER_NAV.dataset.current) + 1}.jpg`;
+
     img.src = url;
     img.onload = () => {
       document.body.style.backgroundImage = `url(${url})`;
