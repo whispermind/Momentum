92ae11e4818881ea425bd2865f3657d46c95d962
whispermind <49441048+whispermind@users.noreply.github.com>
2021-10-27 06:51:12 +0300

    fix: tags area opacity to storage

--- momentum/scripts/state.js
+++ momentum/scripts/state.js
@@ -55,6 +55,7 @@ export default function stateChange() {
   function setStorage() {
     document.querySelector(`option[value=${settings.lang}]`).selected = true;
     document.querySelector(`option[value=${settings.api}]`).selected = true;
+    if (settings.api !== 'git') document.querySelector('label[for="tags"]').style.opacity = 1;
     document.querySelector('#tags').value = settings.tags;
     document.querySelector('#player').checked = settings.player;
     document.querySelector('#time').checked = settings.time;
