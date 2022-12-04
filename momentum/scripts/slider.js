7c9c9279e90466c7782571c60552d3cecf181db3
whispermind <49441048+whispermind@users.noreply.github.com>
2022-12-04 14:26:43 +0200

    fix: add fetch data-length validation

--- momentum/scripts/slider.js
+++ momentum/scripts/slider.js
@@ -1,38 +1,71 @@
 import { getDayTime } from "./greetings.js";
-const SLIDER_NAV = document.querySelector('.slider-icons');
+const SLIDER_NAV = document.querySelector(".slider-icons");
 let unsplash = null;
 let flickr = null;
 export default async function slider(update) {
-  const { settings } = await import('./state.js');
-  if (settings.api === 'unsplash') unsplash = await getData(`https://api.unsplash.com/search/photos/?client_id=SaIx04gu2ik9Jsm_ExeT4Ah7u5sXqhPnd7bo-oZouyA&orientation=landscape&query=${settings.tags ? settings.tags : await getDayTime()}&per_page=20`);
-  if (settings.api === 'flickr') flickr = await getData(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f1d1e25e63b77ed91af6b5fe0b0a65ac&tags=${settings.tags ? settings.tags : await getDayTime()}&extras=url_l&format=json&nojsoncallback=1&per_page=20`);
+  const { settings } = await import("./state.js");
+  if (settings.api === "unsplash") {
+    const data = await getData(
+      `https://api.unsplash.com/search/photos/?client_id=SaIx04gu2ik9Jsm_ExeT4Ah7u5sXqhPnd7bo-oZouyA&orientation=landscape&query=${
+        settings.tags ? settings.tags : await getDayTime()
+      }&per_page=20`
+    );
+    if (data.results.length) unsplash = data;
+    else alert("there are no data for requested tags");
+  }
+  if (settings.api === "flickr") {
+    const data = await getData(
+      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f1d1e25e63b77ed91af6b5fe0b0a65ac&tags=${
+        settings.tags ? settings.tags : await getDayTime()
+      }&extras=url_l&format=json&nojsoncallback=1&per_page=20`
+    );
+    if (data.photos.photo.length) flickr = data;
+    else alert("there are no data for requested tags");
+  }
   let processed = false;
   if (!update) sliderInit();
 
   async function setImage() {
     const flickrCollection = flickr?.photos.photo;
     const unsplashCollection = unsplash?.results;
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     let url = null;
-    if (settings.api === 'unsplash') url = unsplashCollection[SLIDER_NAV.dataset.current].urls.regular;
-    if (settings.api === 'flickr') url = flickrCollection[SLIDER_NAV.dataset.current].url_l;
-    if (settings.api === 'git') url = `https://raw.githubusercontent.com/whispermind/stage1-tasks/assets/images/${await getDayTime()}/${SLIDER_NAV.dataset.current < 9 ? '0' + (Number(SLIDER_NAV.dataset.current) + 1) : Number(SLIDER_NAV.dataset.current) + 1}.jpg`;
+    if (settings.api === "unsplash" && unsplashCollection.length)
+      url = unsplashCollection[SLIDER_NAV.dataset.current].urls.regular;
+    if (settings.api === "flickr" && flickrCollection.length)
+      url = flickrCollection[SLIDER_NAV.dataset.current].url_l;
+    if (settings.api === "git")
+      url = `https://raw.githubusercontent.com/whispermind/stage1-tasks/assets/images/${await getDayTime()}/${
+        SLIDER_NAV.dataset.current < 9
+          ? "0" + (Number(SLIDER_NAV.dataset.current) + 1)
+          : Number(SLIDER_NAV.dataset.current) + 1
+      }.jpg`;
 
     img.src = url;
     img.onload = () => {
       document.body.style.backgroundImage = `url(${url})`;
-      setTimeout(() => processed = false, 1500);
+      setTimeout(() => (processed = false), 1500);
     };
   }
   function sliderInit() {
-    SLIDER_NAV.dataset.current = getRandomInt(0, 19);
-    SLIDER_NAV.addEventListener('click', async function (event) {
-      if (processed) return
+    const dataLength =
+      settings.api === "unsplash"
+        ? unsplash?.results.length
+        : settings.api === "flickr"
+        ? flickr?.photos.photo.length
+        : 19;
+    SLIDER_NAV.dataset.current = getRandomInt(0, dataLength);
+    SLIDER_NAV.addEventListener("click", async function (event) {
+      if (processed) return;
       processed = true;
-      if (event.target.classList.contains('slide-prev')) SLIDER_NAV.dataset.current--
-      if (event.target.classList.contains('slide-next')) SLIDER_NAV.dataset.current++
-      if (SLIDER_NAV.dataset.current > 19) SLIDER_NAV.dataset.current = 0;
-      if (SLIDER_NAV.dataset.current < 0) SLIDER_NAV.dataset.current = 19;
+      if (event.target.classList.contains("slide-prev"))
+        SLIDER_NAV.dataset.current--;
+      if (event.target.classList.contains("slide-next"))
+        SLIDER_NAV.dataset.current++;
+      if (SLIDER_NAV.dataset.current > dataLength)
+        SLIDER_NAV.dataset.current = 0;
+      if (SLIDER_NAV.dataset.current < 0)
+        SLIDER_NAV.dataset.current = dataLength - 1;
       setImage();
     });
     setImage();
--- momentum/scripts/state.js
+++ momentum/scripts/state.js
@@ -6,9 +6,9 @@ import quotes from "./quotes.js";
 import player from "./player.js";
 
 export let settings = {
-  lang: 'english',
-  api: 'git',
-  tags: '',
+  lang: "english",
+  api: "git",
+  tags: "",
   player: true,
   time: true,
   quotes: true,
@@ -17,11 +17,11 @@ export let settings = {
   date: true,
 };
 export default function stateChange() {
-  const CLOSE = document.querySelector('.state-close');
-  const OPEN = document.querySelector('.state-open');
-  const STATE = document.querySelector('.state-options');
+  const CLOSE = document.querySelector(".state-close");
+  const OPEN = document.querySelector(".state-open");
+  const STATE = document.querySelector(".state-options");
   const TAGS = document.querySelector('label[for="tags"]');
-  const STORAGE = localStorage.getItem('settings');
+  const STORAGE = localStorage.getItem("settings");
   if (STORAGE) {
     settings = JSON.parse(STORAGE);
     setStorage();
@@ -30,42 +30,77 @@ export default function stateChange() {
     set: function (target, key, value) {
       target[key] = value;
       switch (key) {
-        case 'lang': weather(true); greetings(true); break;
-        case 'api': slider(true); break;
-        case 'tags': slider(true); break;
-        case 'player': player(true); break;
-        case 'time': timer(true); break;
-        case 'date': timer(true); break;
-        case 'quotes': quotes(true); break;
-        case 'weather': weather(true); break;
-        case 'greetings': greetings(true); break;
+        case "lang":
+          weather(true);
+          greetings(true);
+          break;
+        case "api":
+          slider(true);
+          break;
+        case "tags":
+          slider(true);
+          break;
+        case "player":
+          player(true);
+          break;
+        case "time":
+          timer(true);
+          break;
+        case "date":
+          timer(true);
+          break;
+        case "quotes":
+          quotes(true);
+          break;
+        case "weather":
+          weather(true);
+          break;
+        case "greetings":
+          greetings(true);
+          break;
       }
       return true;
-    }
+    },
   });
 
-  OPEN.addEventListener('click', event => { STATE.style.transition = '1s ease-in-out'; STATE.style.right = '0' });
-  CLOSE.addEventListener('click', event => STATE.style.right = '');
-  STATE.addEventListener('change', event => {
-    OBSERVER[event.target.id] = event.target.value === 'on' ? event.target.checked : event.target.value;
-    if (settings.api !== 'git') { TAGS.style.opacity = 1; TAGS.querySelector('input').disabled = false; }
-    else { TAGS.style.opacity = ''; TAGS.readonly = ''; TAGS.querySelector('input').disabled = true; };
-    localStorage.setItem('settings', JSON.stringify(settings));
+  OPEN.addEventListener("click", (event) => {
+    STATE.style.transition = "1s ease-in-out";
+    STATE.style.right = "0";
+  });
+  CLOSE.addEventListener("click", (event) => (STATE.style.right = ""));
+  STATE.addEventListener("change", (event) => {
+    OBSERVER[event.target.id] =
+      event.target.value === "on" ? event.target.checked : event.target.value;
+    if (event.target.id === "player") {
+      const audio = document.querySelector("audio");
+      const button = document.querySelector(".pause");
+      audio.pause();
+      button.classList.remove("pause");
+    }
+    if (settings.api !== "git") {
+      TAGS.style.opacity = 1;
+      TAGS.querySelector("input").disabled = false;
+    } else {
+      TAGS.style.opacity = "";
+      TAGS.readonly = "";
+      TAGS.querySelector("input").disabled = true;
+    }
+    localStorage.setItem("settings", JSON.stringify(settings));
   });
   function setStorage() {
     document.querySelector(`option[value=${settings.lang}]`).selected = true;
     document.querySelector(`option[value=${settings.api}]`).selected = true;
-    if (settings.api !== 'git') {
+    if (settings.api !== "git") {
       let tag = document.querySelector('label[for="tags"]');
       tag.style.opacity = 1;
-      tag.querySelector('input').disabled = false;
+      tag.querySelector("input").disabled = false;
     }
-    document.querySelector('#tags').value = settings.tags;
-    document.querySelector('#player').checked = settings.player;
-    document.querySelector('#time').checked = settings.time;
-    document.querySelector('#date').checked = settings.date;
-    document.querySelector('#quotes').checked = settings.quotes;
-    document.querySelector('#weather').checked = settings.weather;
-    document.querySelector('#greetings').checked = settings.greetings;
+    document.querySelector("#tags").value = settings.tags;
+    document.querySelector("#player").checked = settings.player;
+    document.querySelector("#time").checked = settings.time;
+    document.querySelector("#date").checked = settings.date;
+    document.querySelector("#quotes").checked = settings.quotes;
+    document.querySelector("#weather").checked = settings.weather;
+    document.querySelector("#greetings").checked = settings.greetings;
   }
 }
